781ad43ea45706b7b2ae3ccda7272c7fb32edd50
JayJayBinks <profikerl@yahoo.de>
2024-08-16 12:48:47 +0200

    move analytics to root page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+    injectSpeedInsights();
+    inject({ mode: dev ? 'development' : 'production' });
+</script>
+
