ff5c71663c8ea82a0cd089f882464d2c446ed96b
JayJayBinks <profikerl@yahoo.de>
2024-08-28 18:16:38 +0200

    TODO: revert debug showing action difficulty

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -164,6 +164,8 @@
             action.text += " (" + mpCost + " MP)";
         }
         button.textContent = action.text;
+        //TODO only for debugging, remove before release,
+        button.textContent += "   " + action.action_difficulty;
         if (!isEnoughMP) {
             button.disabled = true;
         }
