1f79f8a06180bcb015bc67fbf55e0d52264afb32
JayJayBinks <profikerl@yahoo.de>
2024-09-11 17:36:31 +0200

    Use vercel env for analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
-    import {env} from '$env/dynamic/public';
+    import { VERCEL_ENV } from '$env/static/private';
     import {inject} from '@vercel/analytics';
     import {injectSpeedInsights} from '@vercel/speed-insights/sveltekit';
 
-    const mode = env.VERCEL_ENV || 'development';
-    console.log('VERCEL_ENV is', env.VERCEL_ENV);
+    const mode = VERCEL_ENV || 'development';
+    console.log('VERCEL_ENV is', VERCEL_ENV);
     if (mode === 'production') {
         injectSpeedInsights();
         inject({mode: 'production'});
