056d813cf3b5294ef1076da332283975ee034668
JayJayBinks <profikerl@yahoo.de>
2025-02-02 14:56:32 +0100

    fix scroll bars for gm question

--- src/lib/components/interaction_modals/GMQuestionModal.svelte
+++ src/lib/components/interaction_modals/GMQuestionModal.svelte
@@ -65,14 +65,14 @@
 	<LoadingModal />
 {:else}
 	<dialog open class="z-100 modal" style="background: rgba(0, 0, 0, 0.3);">
-		<div class="modal-box flex flex-col items-center text-center overflow-y-scroll">
+		<div class="modal-box flex flex-col items-center text-center">
 			<span class="m-auto font-bold">Game Master Answer</span>
 			<p class="max-h-48 mt-4 overflow-y-scroll">{gmAnswerState?.answerToPlayer}</p>
 			<details class="collapse collapse-arrow textarea-bordered border bg-base-200 mt-4 overflow-y-scroll">
 				<summary class="collapse-title capitalize">
 					<p>Considered Game State</p>
 				</summary>
-				<p class="overflow-y-scroll">{gmAnswerState?.game_state_considered || 'The AI did not return a response...'}</p>
+				<p>{gmAnswerState?.game_state_considered || 'The AI did not return a response...'}</p>
 			</details>
 			<details class="collapse collapse-arrow textarea-bordered border bg-base-200 mt-4 overflow-y-scroll">
 				<summary class="collapse-title capitalize">
