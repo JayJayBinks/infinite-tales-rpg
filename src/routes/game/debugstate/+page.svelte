27f91467a67510067bb21cd39eb8bbabb133e374
JayJayBinks <profikerl@yahoo.de>
2025-08-22 20:35:07 +0200

    display current simulation output in debug

--- src/routes/game/debugstate/+page.svelte
+++ src/routes/game/debugstate/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { initialThoughtsState, stringifyPretty, type ThoughtsState } from '$lib/util.svelte';
 	import { useLocalStorage } from '$lib/state/useLocalStorage.svelte';
+	import type { LLMMessage } from '$lib/ai/llm';
 
 	const gameActionsState = useLocalStorage('gameActionsState');
 	const npcState = useLocalStorage('npcState', {});
 	const characterActionsState = useLocalStorage('characterActionsState', {});
+	const historyMessagesState = useLocalStorage<LLMMessage[]>('historyMessagesState');
 	let thoughtsState = useLocalStorage<ThoughtsState>('thoughtsState', initialThoughtsState);
 </script>
 
@@ -13,6 +15,13 @@
 		<p class="text-center">NPC State</p>
 	</summary>
 	<output style="white-space: pre-wrap">{stringifyPretty(npcState.value)}</output>
+</details>
+
+<details class="menu collapse collapse-arrow menu-vertical mt-7 bg-base-200">
+	<summary class="collapse-title text-lg font-bold capitalize">
+		<p class="text-center">Current Simulation</p>
+	</summary>
+	<output style="white-space: pre-wrap">{historyMessagesState.value?.findLast((message) => message.role === 'user')?.content || "No simulation"}</output>
 </details>
 
 <details class="menu collapse collapse-arrow menu-vertical mt-7 bg-base-200">
