3c8b189e004ae91e3d08d05461542be23b782b6a
Julian Wrastil <julian.wrastil@holisticon.de>
2025-08-29 12:27:07 +0200

    Fix game update vlue mapping

--- src/lib/ai/agents/mappers.ts
+++ src/lib/ai/agents/mappers.ts
@@ -14,11 +14,17 @@ export function mapStatsUpdates(object: Pick<GameActionState, 'stats_update'>) {
 
 export function mapStatsUpdate(stats_update): StatsUpdate {
 	let parsed: DiceRoll;
+
+	function tryMapAnyResult(stats_update) {
+		return { result: Number.parseInt(stats_update.value) || Number.parseInt(stats_update.value?.result) || Number.parseInt(stats_update.value?.result?.result)
+				|| stats_update.value };
+	}
+
 	try {
 		parsed = Dice.detailed(stats_update.value);
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (e) {
-		parsed = { result: Number.parseInt(stats_update.value) || Number.parseInt(stats_update.value?.value) || stats_update.value?.value || stats_update.value };
+		parsed = tryMapAnyResult(stats_update);
 	}
 	return { ...stats_update, value: parsed };
 }
